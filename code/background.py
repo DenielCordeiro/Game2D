2aa57f6763ee84ca1b38b38adac34fa5f258d94e
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2026-05-15 18:21:52 -0300

    refactor[Background]: I imported os to work with the file path file

--- code/background.py
+++ code/background.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from const import WINDOW_GAME_HEIGHT, WINDOW_GAME_WIDTH
 
@@ -7,9 +8,10 @@ class Background:
         self.rect = self.buildRect(position)
 
     def loadImages(self, name: str):
-        self.surf = pygame.image.load('../assets/background/' + name + '.png').convert_alpha() # carrega a imagem do background
+        current_dir = os.path.dirname(__file__) # obtém o diretório atual do arquivo background.py
+        image_path = os.path.join(current_dir, '..', 'assets', 'background', f'{name}.png') # constrói o caminho completo para a imagem do background
+        self.surf = pygame.image.load(image_path).convert_alpha() # carrega a imagem do background
         self.surf = pygame.transform.scale(self.surf, (WINDOW_GAME_WIDTH, WINDOW_GAME_HEIGHT)) # redimensiona a imagem do background para o tamanho da janela do jogo
-
 
     def buildRect(self, position: tuple):
         return self.surf.get_rect(left=position[0], top=position[1]) # cria um retângulo para o background com a posição especificada
